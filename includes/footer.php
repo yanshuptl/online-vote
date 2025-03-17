82d6aa87505a0dddd1686612b0478cc56b02b48f
yanshuptl <152694638+yanshuptl@users.noreply.github.com>
2025-03-18 02:58:48 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+  <div class="container">
+    <div class="pull-right hidden-xs">
+      <b>Group-20</b>
+    </div>
+    <strong>Copyright &copy; 2024 <a href="">DecoderDeep</a></strong>
+  </div>
+  <!-- /.container -->
+</footer>
